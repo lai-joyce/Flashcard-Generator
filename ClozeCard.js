c4550631422eb6f45f48de7a59c51a077d05f78a
lai-joyce <lai.joyce@gmail.com>
2017-07-17 23:20:45 -0700

    draft 5

--- ClozeCard.js
+++ ClozeCard.js
@@ -13,20 +13,20 @@ var clozeFlash = function(text, cloze) {
     this.text = text;
     this.cloze = cloze;
     this.clozeDeleted = text.replace(cloze, '...');
-    // this.generate = function() {
-    //     var questions = {
-    //         text: this.text,
-    //         cloze: this.cloze,
-    //         clozeDeleted: this.clozeDeleted,
-    //         form: "cloze"
-    //     };
-    //     fs.appendFile("log.txt", JSON.stringify(questions) + ';', "utf8", function(error) {
-    //         // if there is an error, log the error
-    //         if (error) {
-    //             console.log(error);
-    //         }
-    //     });
-    // };
+    this.generate = function() {
+        var questions = {
+            text: this.text,
+            cloze: this.cloze,
+            clozeDeleted: this.clozeDeleted,
+            form: "cloze"
+        };
+        fs.appendFile("log.txt", JSON.stringify(questions) + ';', "utf8", function(error) {
+            // if there is an error, log the error
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
 }
 
 module.exports = clozeFlash;
--- logic.js
+++ logic.js
@@ -17,6 +17,114 @@ var onQuestion = 0;
 var rightAnswers = 0;
 var wrongAnswers = 0;
 
+// function startQuestion() {
+	inquirer.prompt([
+	{
+		name: "userInput"
+		message: "Would you like to add card or play game?",
+		type: "list",
+		choices: [{
+			name: "create card"
+		}, {
+			name: "play game"
+		}]
+	}
+	]).then(function(answer) {
+		if(answer.userInput === "create card") {
+			createCard();
+		} else {
+			askQuestion();
+		}
+	});
+// }
+
+function createCard() {
+	inquirer.prompt([
+	{
+		name: "version",
+		type: "list",
+		message: "Would you like to create a basic card or cloze card?",
+		choices: [{
+			name: "basic card"
+		}, {
+			name: "cloze card"
+		}]
+	}
+	]).then(function(answer) {
+		if(answer.version === "basic card") {
+			inquirer.prompt([
+			{
+				name: "front",
+				message: "What is the question?",
+				validate: function(input) {
+					if(input === " ") {
+						console.log("Please enter a question.");
+						return false;
+					} else {
+						return true;
+					}
+				}
+			}, {
+				name: "back",
+				message: "What is the answer?",
+				validate: function(input) {
+					if(input === " ") {
+						console.log("Please enter an answer.");
+						return false;
+					} else {
+						return true;
+					}
+				}
+			}
+			]).then(function(answer) {
+				var newBasicCard = new basicFlash(answer.front,answer.back);
+				newBasicCard.generate();
+				startQuestion();
+			});
+			
+		} else {
+			inquirer.prompt([
+			{
+				name: "text",
+				message: "What is the complete text?",
+				validate: function(input) {
+					if(input === " ") {
+						console.log("Please enter the complete text.");
+						return false;
+					} else {
+						return true;
+					}
+				}
+
+			}, {
+				name: "cloze",
+				message: "What is the cloze text?",
+				validate: function(input) {
+					if(input === " ") {
+						console.log("Please enter the cloze text.");
+						return false;
+					} else {
+						return true;
+					}
+				}
+
+			}
+			]).then(function(answer) {
+				var text = answer.text;
+				var cloze = answer.cloze;
+				if(text.includes(cloze)) {
+					var newClozeCard = new clozeFlash(text,cloze);
+					newClozeCard.generate();
+					startQuestion();
+				} else {
+					console.log("The cloze portion is not in your sentence. Try again.");
+					createCard();
+				}
+			});
+		}
+	});
+}
+
 function askQuestion() {
 	inquirer.prompt([
 	{
@@ -70,4 +178,6 @@ function askQuestion() {
 }
 
 askQuestion();
+
+// startQuestion();
 
